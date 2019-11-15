dc02b2f5d16be7b3ac6e1bc770a28e430c2753ca
bpopovschi <zyqsempai@mail.ru>
2019-11-15 14:00:55 +0200

    Header fix
    Signed-off-by: bpopovschi <zyqsempai@mail.ru>

--- v2/devicefilter.go
+++ v2/devicefilter.go
@@ -12,16 +12,6 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-
-	devicefilter containes eBPF device filter program
-
-	The implementation is based on https://github.com/containers/crun/blob/0.10.2/src/libcrun/ebpf.c
-
-	Although ebpf.c is originally licensed under LGPL-3.0-or-later, the author (Giuseppe Scrivano)
-	agreed to relicense the file in Apache License 2.0: https://github.com/opencontainers/runc/issues/2144#issuecomment-543116397
-
-	This particular Go implementation based on runc version
-	https://github.com/opencontainers/runc/blob/master/libcontainer/cgroups/ebpf/devicefilter/devicefilter.go
 */
 
 package v2
@@ -36,6 +26,17 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+/*
+Devicefilter containes eBPF device filter program
+
+The implementation is based on https://github.com/containers/crun/blob/0.10.2/src/libcrun/ebpf.c
+
+Although ebpf.c is originally licensed under LGPL-3.0-or-later, the author (Giuseppe Scrivano)
+agreed to relicense the file in Apache License 2.0: https://github.com/opencontainers/runc/issues/2144#issuecomment-543116397
+
+This particular Go implementation based on runc version
+https://github.com/opencontainers/runc/blob/master/libcontainer/cgroups/ebpf/devicefilter/devicefilter.go
+*/
 const (
 	// license string format is same as kernel MODULE_LICENSE macro
 	license = "Apache"
